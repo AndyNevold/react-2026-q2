1b472f8d31414191e7dd9dcad7488b0ff740daa5
andynevold <andynevold@gmail.com>
2026-05-17 21:41:56 +0300

    fix: restore small loader dots inside cards

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // base: '/',
-  base: '/react-2026-q2/',
+  base: '/',
+  // base: '/react-2026-q2/',
   plugins: [react()],
 });
