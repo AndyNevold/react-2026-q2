9d92ba3e010a10504eaa9d61a5592e134ac673ae
andynevold <andynevold@gmail.com>
2026-05-18 22:43:45 +0300

    feat: add author info

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -6,6 +6,16 @@ export function AboutPage(): ReactNode {
       <h1>About</h1>
       <p>Pokemon Search App</p>
       <p>
+        Author:{' '}
+        <a
+          href="https://github.com/andynevold"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          AndyNevold
+        </a>
+      </p>
+      <p>
         Created as part of the{' '}
         <a
           href="https://rs.school/courses/reactjs"
